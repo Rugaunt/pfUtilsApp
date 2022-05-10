b08da1528a62ea29c39c037a04d463ae69e1c2c7
Rugaunt <102379986+Rugaunt@users.noreply.github.com>
2022-05-10 10:39:12 -0600

    cleanup code
    cleaned up import experiment for readability

--- pf-util/src/components/itemDisplayBox.jsx
+++ pf-util/src/components/itemDisplayBox.jsx
@@ -5,12 +5,13 @@ class ItemDisplayBox extends Component {
     state = {  } 
     render() { 
         return (
-            <body lang="en-CA" link="#000080" vlink="#800000" dir="ltr"><p style="margin-bottom: 0cm; line-height: 100%">
-Generated Item</p>
-<p style="margin-bottom: 0cm; line-height: 100%">ItemName		GPValue</p>
-<p style="margin-bottom: 0cm; line-height: 100%">Magic
-Level		SlotType</p>
-</body>
+            <body lang="en-CA" link="#000080" vlink="#800000" dir="ltr">
+                <p style="margin-bottom: 0cm; line-height: 100%">
+                    Generated Item</p>
+                <p style="margin-bottom: 0cm; line-height: 100%">ItemName		GPValue</p>
+                <p style="margin-bottom: 0cm; line-height: 100%">Magic
+                    Level		SlotType</p>
+            </body>
         );
     }
 }
